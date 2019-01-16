d856f6ccd82c072aa1c15c46c35cda49e8256069
FrozenMorozen <morozovd96@outlook.com>
2019-01-16 21:35:42 +0700

    Create architecture

--- testTask/src/main/java/com/testTask/testTask/TestTaskApplication.java
+++ testTask/src/main/java/com/testTask/testTask/TestTaskApplication.java
@@ -1,7 +1,9 @@
 package com.testTask.testTask;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class TestTaskApplication {
