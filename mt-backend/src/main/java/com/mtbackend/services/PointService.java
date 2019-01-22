19545677d99fd1acf2f086c4df789b6285d17a94
Alexander Marshalov <_@marshalov.org>
2019-01-23 02:03:12 +0700

    Edit backend

--- mt-backend/src/main/java/com/mtbackend/services/PointService.java
+++ mt-backend/src/main/java/com/mtbackend/services/PointService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface PointService {
 
-    Iterable<Point> findByNameContainingIgnoreCase(String name);
+    Iterable<Point> findPointByFilter(String name);
 
     void add(Point point);
 
